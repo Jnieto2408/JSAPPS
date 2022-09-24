83be9f202dae647ca83245ff550cfc736b6bdfcc
jnieto2408 <jnieto2408@gmail.com>
2022-09-24 18:51:55 -0500

    fixing entries in passenger app

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let count = 0;
+let saves = 0;
 console.log(count);
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
+
 function increment(){
     count = count + 1;
     console.log(count);
@@ -13,14 +15,22 @@ function decrease(){
     countEl.innerText = count;
 };
 function save(){
-    let countStr = count + " / ";
-    saveEl.textContent += countStr;
-    count = 0;
+    if (saves == 0){
+        let countStr = count;
+        saveEl.textContent += countStr;
+        saves = 1;
+        count = 0;
+    } else {
+        let countStr = " / " + count;
+        saveEl.textContent += countStr;
+        count = 0;
+    }
     countEl.innerText = count;
 };
 function reset(){
     let countStr = "Previous entries: ";
     saveEl.textContent = countStr;
     count = 0;
+    saves = 0;
     countEl.innerText = count;
 };
