abf0f16c01672d4f430c6524e529cdd9660be8fd
jnieto2408 <jnieto2408@gmail.com>
2022-10-08 19:01:22 -0500

    fixing/adding things in html and js guessnumber app

--- javascript/guessnumber.js
+++ javascript/guessnumber.js
@@ -1,31 +1,36 @@
 /* Guess Number APP */
 let tries = 3;
-let statusGuess = document.getElementById("status-guess");
-let infoGuess = document.getElementById("info-guess");
-let motGuess = document.getElementById("mot-guess");
+let startToken = 1;
+const statusGuess = document.getElementById("statusGuess");
+const infoGuess = document.getElementById("infoGuess");
+const mottoGuess = document.getElementById("mottoGuess");
 let rndNumber = Math.floor(Math.random() * 10) + 1;
-const resetter = 3;
-function startGuess(){
-    if(tries == 3){
+const startGuess = document.getElementById("startGuess");
+startGuess.onclick = () => {
+    if((tries == 3) && (startToken = 1)){
     statusGuess.innerText = ("Lets start! You have " + tries + " oportunities left");
     console.log(rndNumber);
+    startToken = 0;
     } else {
         alert("Reset to start over");
     }
-}
-function resetGuess(){
+};
+const resetGuess = document.getElementById("resetGuess");
+resetGuess.onclick = () => {
     tries = 3;
+    startToken = 1;
     statusGuess.innerText = ("");
     infoGuess.innerText = ("");
     rndNumber = Math.floor(Math.random() * 10) + 1;
     console.log(rndNumber);
-}
-function tryGuess(){
-    let numberElection = document.getElementById("number-guess").value;
-    console.log(numberElection);
+};
+const tryGuess = document.getElementById("tryGuess");
+tryGuess.onclick = () => {
+    const numberGuess = document.getElementById("numberGuess").value;
+    console.log(numberGuess);
     tries = tries - 1;
-    if(tries >= 0){
-        if(numberElection == rndNumber){
+    if(tries >= 1){
+        if(numberGuess == rndNumber){
         infoGuess.innerText = ("CONGRATULATIONS YOU WIN");
         } else {
         statusGuess.innerText = ("Lets start! You have " + tries + " oportunities left");
@@ -34,4 +39,4 @@ function tryGuess(){
     } else{
         infoGuess.innerText = ("You ran out of opportunities, reset to try again");
     }
-}
+};
