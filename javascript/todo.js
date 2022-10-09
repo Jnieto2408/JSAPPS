87d942226ecb2b8bb90b6380f3a1120e887bc4b1
jnieto2408 <jnieto2408@gmail.com>
2022-10-08 20:32:57 -0500

    fixing structure ToDo

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,1 +1,14 @@
 /* To-Do APP */
+class Todo {
+    constructor(note, status, proiority, date){
+        this.note = note;
+        this.status = status;
+        this.proiority = proiority;
+        this.date = date;
+    }
+}
+const arrayTodo = [];
+const formTodo = document.getElementById("formTodo");
+formTodo.addEventListener("submit",(e) => {
+    e.preventDefault();
+})
