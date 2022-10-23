6fd9fc343e1b27da7bf4c69f53b7f5f7057db8d8
jnieto2408 <jnieto2408@gmail.com>
2022-10-23 13:48:25 -0500

    adding basic funcionality to calculator, missing operations

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -15,4 +15,59 @@ const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
 const min = document.getElementById("min");
 const period = document.getElementById("period");
-const result = document.getElementById("zero");
+const zero = document.getElementById("zero");
+const result = document.getElementById("result");
+
+let currentNumber = "";
+one.addEventListener("click", () => {
+    currentNumber += 1;
+    console.log(currentNumber);
+});
+two.addEventListener("click", () => {
+    currentNumber += 2;
+    console.log(currentNumber);
+});
+three.addEventListener("click", () => {
+    currentNumber += 3;
+    console.log(currentNumber);
+});
+four.addEventListener("click", () => {
+    currentNumber += 4;
+    console.log(currentNumber);
+});
+five.addEventListener("click", () => {
+    currentNumber += 5;
+    console.log(currentNumber);
+});
+six.addEventListener("click", () => {
+    currentNumber += 6;
+    console.log(currentNumber);
+});
+seven.addEventListener("click", () => {
+    currentNumber += 7;
+    console.log(currentNumber);
+});
+eight.addEventListener("click", () => {
+    currentNumber += 8;
+    console.log(currentNumber);
+});
+nine.addEventListener("click", () => {
+    currentNumber += 9;
+    console.log(currentNumber);
+});
+zero.addEventListener("click", () => {
+    currentNumber += 0;
+    console.log(currentNumber);
+});
+period.addEventListener("click", () => {
+    currentNumber += ".";
+    console.log(currentNumber);
+});
+ac.addEventListener("click", () => {
+    currentNumber = "";
+    console.log(currentNumber);
+});
+del.addEventListener("click", () => {
+    currentNumber = currentNumber.slice(0, -1);
+    console.log(currentNumber);
+});
