7dfc82edd0cb2d9fb119458d0df9539c4a34ba49
jnieto2408 <jnieto2408@gmail.com>
2022-10-27 23:18:19 -0500

    game structure defined

--- javascript/blackjack.js
+++ javascript/blackjack.js
@@ -1,25 +1,14 @@
 /* BlackJack APP */
 const start = document.getElementById("start");
+const remove = document.getElementById("remove");
 const game = document.getElementById("game");
 start.addEventListener("click", () => {
     start.style.display= "none";
+    remove.style.display= "none";
     startGame();
 })
 const startGame = () => {
     game.innerHTML += `
-                        <div class="headGame">
-                            <div class="money">
-                                <p>Money:</p>
-                                <p class="cash">$1000</p>
-                            </div>
-                            <div class="lifes">
-                                <div>Lifes</div>
-                                <div class="hearts">
-                                    <i class="fa fa-heart" id="h1"></i>
-                                    <i class="fa fa-heart" id="h2"></i>
-                                    <i class="fa fa-heart" id="h3"></i>
-                                </div>
-                            </div>
-                        </div>    
+
     `
 };
