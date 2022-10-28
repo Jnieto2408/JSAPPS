afa7265ba26403ebc462877e14c0634c0c95f545
jnieto2408 <jnieto2408@gmail.com>
2022-10-27 22:08:47 -0500

    adding game structure html blackjack

--- javascript/blackjack.js
+++ javascript/blackjack.js
@@ -7,6 +7,19 @@ start.addEventListener("click", () => {
 })
 const startGame = () => {
     game.innerHTML += `
-    
+                        <div class="headGame">
+                            <div class="money">
+                                <p>Money:</p>
+                                <p class="cash">$1000</p>
+                            </div>
+                            <div class="lifes">
+                                <div>Lifes</div>
+                                <div class="hearts">
+                                    <i class="fa fa-heart" id="h1"></i>
+                                    <i class="fa fa-heart" id="h2"></i>
+                                    <i class="fa fa-heart" id="h3"></i>
+                                </div>
+                            </div>
+                        </div>    
     `
 };
